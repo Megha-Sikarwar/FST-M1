b15456c3866f3a31e2c6412ce482b29e126cecdf
Megha-Sikarwar <megha.sikarwar@ibm.com>
2024-12-26 20:00:52 +0530

    Delete Java/Activities/NotEnoughFundsException.java

--- Java/Activities/NotEnoughFundsException.java
+++
@@ -1,7 +0,0 @@
-package tes.java;
-
-public class NotEnoughFundsException extends RuntimeException{
-	public NotEnoughFundsException(Integer amount, Integer balance) {
-        super("Attempted to withdraw " + amount + " with a balance of " + balance);
-    }
-}
